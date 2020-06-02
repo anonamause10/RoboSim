25082ee73b6ead47237d9c79da27b1ede24aeeae
anonamause10 <anonamause10@gmail.com>
2020-06-02 00:56:16 -0500

    added some walls

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+
+public class CameraMovement : MonoBehaviour {
+
+	public float turnSpeed = 4.0f;
+ 
+	public GameObject target;
+	private float targetDistance;
+	float targetDistanceInitial;
+	float speedSmoothVelocity = 1f;
+	
+	
+	public float minTurnAngle = -90.0f;
+	public float maxTurnAngle = 0.0f;
+	private float rotX;
+	
+	void Start ()
+	{
+	    targetDistance = Vector3.Distance(transform.position, target.transform.position);
+		targetDistanceInitial = targetDistance;
+	}
+	
+	void LateUpdate ()
+	{
+		Move();
+
+	}
+
+	void Move(){
+	    // get the mouse inputs
+	    float y = Input.GetAxis("Mouse X") * turnSpeed;
+	    rotX += Input.GetAxis("Mouse Y") * turnSpeed;
+	
+	    // clamp the vertical rotation
+	    rotX = Mathf.Clamp(rotX, minTurnAngle, maxTurnAngle);
+		targetDistance = Mathf.SmoothDamp (targetDistance, targetDistanceInitial, ref speedSmoothVelocity, 0.1f);
+	
+	    // rotate the camera
+	    transform.eulerAngles = new Vector3(-rotX, transform.eulerAngles.y + y, 0);
+	
+	    // move the camera position
+		transform.position = (target.transform.position) - (transform.forward * targetDistance);
+
+	}
+
+}
---
+++ Assets/Scripts/MoveBlock.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBlock : MonoBehaviour
+{
+    public float speed = 30;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 moveDir = Vector3.ProjectOnPlane(Camera.main.transform.forward,Vector3.up).normalized;
+        transform.Translate(speed*moveDir*Input.GetAxis("Vertical")*Time.deltaTime,Space.World);
+    }
+}
