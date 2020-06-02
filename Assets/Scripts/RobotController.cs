f0d814b00f6af061287af2cbd9488adad8e389d7
anonamause10 <anonamause10@gmail.com>
2020-06-02 17:39:31 -0500

    added robot

---
+++ Assets/Scripts/RobotController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotController : MonoBehaviour
+{
+    public WheelController fRWheel, bRWheel, fLWheel, bLWheel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float power = Input.GetAxis("Vertical");
+        fRWheel.SetForce(power);
+        fLWheel.SetForce(power);
+        bLWheel.SetForce(power);
+        bRWheel.SetForce(power);
+
+    }
+}
---
+++ Assets/Scripts/WheelController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelController : MonoBehaviour
+{
+    public Rigidbody robotBody;
+    public bool isGrounded = false;
+    private float forceMult = 5;
+    public float force;
+    private float targetForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        robotBody = transform.parent.parent.gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        force = Mathf.Lerp(force,targetForce,50*Time.deltaTime);
+        Vector3 forceDir = Vector3.ProjectOnPlane(transform.forward,Vector3.up).normalized;
+        if(isGrounded){
+            robotBody.AddForceAtPosition(forceDir*force*forceMult, transform.position);
+        }
+        print(isGrounded);
+    }
+
+    public void SetForce(float target){
+        targetForce = target;
+    }
+
+    //make sure u replace "floor" with your gameobject name.on which player is standing
+    void OnCollisionEnter(Collision theCollision)
+    {
+        if (theCollision.gameObject.name == "Floor")
+        {
+            isGrounded = true;
+        }
+    }
+    
+    //consider when character is jumping .. it will exit collision.
+    void OnCollisionExit(Collision theCollision)
+    {
+        if (theCollision.gameObject.name == "Floor")
+        {
+            isGrounded = false;
+        }
+    }
+}
