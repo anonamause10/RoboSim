2edb086c13fc54b8eb7bafc47bcd5ff28b410b28
anonamause10 <anonamause10@gmail.com>
2020-06-03 16:49:51 -0500

    added braking

--- Assets/Scripts/RobotController.cs
+++ Assets/Scripts/RobotController.cs
@@ -5,17 +5,24 @@ using UnityEngine;
 public class RobotController : MonoBehaviour
 {
     public WheelController fRWheel, bRWheel, fLWheel, bLWheel;
+    private Rigidbody body;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        body = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        print(body.velocity);
         float power = Input.GetAxis("Vertical");
+        if(power==0){
+            body.drag = 25;
+        }else{
+            body.drag = 0;
+        }
         fRWheel.SetForce(power);
         fLWheel.SetForce(power);
         bLWheel.SetForce(power);
--- Assets/Scripts/WheelController.cs
+++ Assets/Scripts/WheelController.cs
@@ -4,48 +4,39 @@ using UnityEngine;
 
 public class WheelController : MonoBehaviour
 {
-    public Rigidbody robotBody;
-    public bool isGrounded = false;
-    private float forceMult = 5;
+    public WheelCollider wheel;
+    public WheelHit hit;
+    public float forceMult = 10;
+    public float brakeForce = 1f;
     public float force;
     private float targetForce;
 
     // Start is called before the first frame update
     void Start()
     {
-        robotBody = transform.parent.parent.gameObject.GetComponent<Rigidbody>();
+        wheel = GetComponent<WheelCollider>();
+        hit = new WheelHit();
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        force = Mathf.Lerp(force,targetForce,50*Time.deltaTime);
-        Vector3 forceDir = Vector3.ProjectOnPlane(transform.forward,Vector3.up).normalized;
-        if(isGrounded){
-            robotBody.AddForceAtPosition(forceDir*force*forceMult, transform.position);
+        bool hitGround = wheel.GetGroundHit(out hit);
+        print(hit.forwardSlip);
+        if(targetForce==0){
+            wheel.brakeTorque= brakeForce;
+        }else{
+            wheel.brakeTorque = 0;
         }
-        print(isGrounded);
+        
+        force = Mathf.Lerp(force,targetForce,50*Time.deltaTime);
+        
+        wheel.motorTorque = Mathf.Lerp(2*forceMult*targetForce,0,Mathf.Abs(wheel.rpm)/600);
+          
     }
 
     public void SetForce(float target){
         targetForce = target;
     }
 
-    //make sure u replace "floor" with your gameobject name.on which player is standing
-    void OnCollisionEnter(Collision theCollision)
-    {
-        if (theCollision.gameObject.name == "Floor")
-        {
-            isGrounded = true;
-        }
-    }
-    
-    //consider when character is jumping .. it will exit collision.
-    void OnCollisionExit(Collision theCollision)
-    {
-        if (theCollision.gameObject.name == "Floor")
-        {
-            isGrounded = false;
-        }
-    }
 }
